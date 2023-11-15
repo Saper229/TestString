a9e8e144913395af5d919416ba05292d05de5950
Pawel <pawelbernys11@gmail.com>
2023-11-15 09:37:27 +0100

    zadanie

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
 
+    String wartosc = "costam";
+    int wartosc2 = wartosc.length();
 
+    String wartosc3 =wartosc.substring(0,wartosc.length()-2);
+    int wartosc4 = wartosc3.length();
+    String wartosc5 = wartosc.substring(0,2) + wartosc4;
 
-
+    if(wartosc2 > 2){
+        System.out.println(wartosc4);
+        }
 
     }
 }
